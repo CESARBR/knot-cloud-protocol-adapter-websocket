4cd6edfe4dec1ae132902ae17647fc4ab3fc9920
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-11-08 17:17:39 -0300

    Add logger
    Change-Id: I361aee3abf65b6e8a9f3010cbef44faaefc23153

--- esm/data/SettingsFactory.js
+++ esm/data/SettingsFactory.js
@@ -1,10 +1,39 @@
-import config from 'config'; // eslint-disable-line no-unused-vars
+import config from 'config';
+import Joi from 'joi';
+import _ from 'lodash';
 
 import Settings from 'data/Settings';
 
+const levels = ['error', 'warn', 'info', 'verbose', 'debug', 'silly'];
+const loggerSchema = Joi.object().keys({
+  level: Joi.string().valid(levels).required(),
+});
+
 class SettingsFactory {
   create() {
-    return new Settings();
+    const logger = this.loadLoggerSettings();
+    return new Settings(logger);
+  }
+
+  loadLoggerSettings() {
+    const logger = config.get('logger');
+    this.validate('logger', logger, loggerSchema);
+    return logger;
+  }
+
+  validate(propertyName, propertyValue, schema) {
+    const { error } = Joi.validate(propertyValue, schema, { abortEarly: false });
+    if (error) {
+      throw this.mapJoiError(propertyName, error);
+    }
+  }
+
+  mapJoiError(propertyName, error) {
+    const reasons = _.map(error.details, 'message');
+    const formattedReasons = reasons.length > 1
+      ? `\n${_.chain(reasons).map(reason => `- ${reason}`).join('\n').value()}`
+      : reasons[0];
+    return new Error(`Invalid "${propertyName}" property: ${formattedReasons}`);
   }
 }
 
