c648e516a6bdf9cc5d9c6f824f03a3202b3051cf
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-11-09 15:40:32 -0300

    Add server and connection handler
    Change-Id: Id194b6fff531dccd3d8fcc914ad39bbcf8d2c101

---
+++ esm/server/ConnectionHandler.js
@@ -0,0 +1,44 @@
+class ConnectionHandler {
+  constructor(client, logger) {
+    this.client = client;
+    this.logger = logger;
+    this.handlers = {};
+  }
+
+  async start() {
+    this.client.on('message', this.onClientMessage.bind(this));
+    this.client.on('close', this.onClientClose.bind(this));
+    this.client.on('error', this.onClientError.bind(this));
+    this.logger.info('Connected');
+  }
+
+  async onClientMessage(event) {
+    try {
+      const handler = this.getHandler(event.type);
+      this.logger.info(`Handling '${event.type}'`);
+      await handler(event.data);
+    } catch (error) {
+      this.logger.error(`Failed processing client message: ${error.message}`);
+      this.logger.debug('Event:', event);
+    }
+  }
+
+  getHandler(type) {
+    if (!this.handlers[type]) {
+      throw new Error(`Unknown event type '${type}'`);
+    }
+    return this.handlers[type];
+  }
+
+  onClientClose(code, reason) {
+    this.logger.info('Disconnected');
+    this.logger.debug(`Disconnect code: ${code}`);
+    this.logger.debug(`Disconnect reason: ${reason}`);
+  }
+
+  onClientError(error) {
+    this.logger.error(error.message);
+  }
+}
+
+export default ConnectionHandler;
