9cd8510bcc90cc544c2ea7ad5b8c3291216de77b
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-11-20 19:50:59 -0300

    Add identity handler
    Change-Id: I07f8cb44bce16b6bc21e01781a32773cfe7baa14

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import SettingsFactory from 'data/SettingsFactory';
+import SessionStore from 'data/SessionStore';
 import LoggerFactory from 'LoggerFactory';
 import CloudRequesterFactory from 'network/CloudRequesterFactory';
 import UuidAliasResolverFactory from 'network/UuidAliasResolverFactory';
@@ -10,10 +11,17 @@ async function main() {
   try {
     const settings = new SettingsFactory().create();
     const loggerFactory = new LoggerFactory(settings);
+
+    const sessionStore = new SessionStore();
+
     const cloudRequester = new CloudRequesterFactory(settings).create();
     const uuidAliasResolver = new UuidAliasResolverFactory(settings).create();
     const cloudFactory = new CloudFactory(cloudRequester, uuidAliasResolver, settings);
-    const connectionHandlerFactory = new ConnectionHandlerFactory(cloudFactory, loggerFactory);
+    const connectionHandlerFactory = new ConnectionHandlerFactory(
+      sessionStore,
+      cloudFactory,
+      loggerFactory,
+    );
     const serverFactory = new ServerFactory(settings, connectionHandlerFactory, loggerFactory);
     const server = serverFactory.create();
 
