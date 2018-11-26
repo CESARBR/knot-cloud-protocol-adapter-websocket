86daed63a379412c7e204ee51a2ad14858917845
João Neto <joao.alexandre@cesar.org.br>
2018-11-26 16:58:14 -0300

    Add register handler
    Change-Id: I69fc50c1b1c4512b2aaa29a9535dbb1e072cf075

---
+++ esm/interactors/RegisterDevice.js
@@ -0,0 +1,93 @@
+class RegisterDevice {
+  constructor(sessionStore, cloud) {
+    this.sessionStore = sessionStore;
+    this.cloud = cloud;
+  }
+
+  async execute(id, properties) {
+    const session = this.sessionStore.get(id);
+    if (!session) {
+      this.generateError('Unauthorized', 401);
+    }
+
+    const device = await this.createDevice(properties, session);
+    const deviceRegistered = await this.cloud.registerDevice(device);
+    return { type: 'deviceRegistered', data: deviceRegistered };
+  }
+
+  async createDevice(properties, session) {
+    const { name, type } = properties;
+    if (!type) {
+      this.generateError('Type property is required');
+    }
+
+    let device = this.createBasicDevice(name, type);
+
+    if (type === 'gateway' || type === 'app') {
+      device = this.createAppOrGatewayDevice(device, session);
+    }
+
+    return device;
+  }
+
+  createBasicDevice(type, name) {
+    return {
+      type,
+      metadata: {
+        name: name || '',
+      },
+    };
+  }
+
+  async createAppOrGatewayDevice(basicDevice, session) {
+    const device = basicDevice;
+    if (!await this.sessionOwnerIsUser(session)) {
+      this.generateError('Device owner isn\'t a user');
+    }
+
+    device.meshblu = {
+      version: '2.0.0',
+      whitelists: this.generateWhitelists(session.uuid),
+    };
+
+    return device;
+  }
+
+  async sessionOwnerIsUser(session) {
+    const deviceOwner = await this.cloud.getDevice(session.credentials.uuid, session.credentials);
+    if (deviceOwner.type !== 'knot:user') {
+      return false;
+    }
+
+    return true;
+  }
+
+  generateError(message, code) {
+    const error = new Error(message);
+    error.code = code;
+    throw error;
+  }
+
+  generateWhitelists(uuid) {
+    return {
+      discover: {
+        as: [{
+          uuid,
+        }],
+        view: [{
+          uuid,
+        }],
+      },
+      configure: {
+        as: [{
+          uuid,
+        }],
+        update: [{
+          uuid,
+        }],
+      },
+    };
+  }
+}
+
+export default RegisterDevice;
