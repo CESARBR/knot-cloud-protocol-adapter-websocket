89501f6d4bca70fb11aa307d41fa948849759e43
Ramon Ribeiro <rhpr@cesar.org.br>
2018-11-27 10:27:03 -0300

    Add unregister handler
    Change-Id: I6950e305489b9166c9c886152f1a43d95365d25a

---
+++ src/interactors/UnregisterDevice.js
@@ -0,0 +1,27 @@
+class UnregisterDevice {
+  constructor(sessionStore, cloud) {
+    this.sessionStore = sessionStore;
+    this.cloud = cloud;
+  }
+
+  async execute(id, data) {
+    const session = this.sessionStore.get(id);
+    const { uuid } = data;
+
+    if (!session) {
+      this.throwError('Unauthorized', 401);
+    }
+
+    await this.cloud.unregister(session.credentials, uuid);
+    this.sessionStore.removeByUuid(uuid);
+    return { type: 'unregistered' };
+  }
+
+  throwError(message, code) {
+    const error = Error(message);
+    error.code = code;
+    throw error;
+  }
+}
+
+export default UnregisterDevice;
