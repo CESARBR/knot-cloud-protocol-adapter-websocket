b39dd0199c581268f7f6b7914f9a9b31e6237427
João Neto <joao.alexandre@cesar.org.br>
2019-01-03 07:19:08 -0300

    Update register handler to support messaging
    This patch is responsible for connecting the gateways and apps with the
    router, enabling the message exchange between them.
    
    Change-Id: Ie8cae0c1723facdc4c4ffb5f3b9553976669631d

--- src/interactors/RegisterDevice.js
+++ src/interactors/RegisterDevice.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 class RegisterDevice {
   constructor(sessionStore, cloud) {
     this.sessionStore = sessionStore;
@@ -10,21 +12,23 @@ class RegisterDevice {
       this.throwError('Unauthorized', 401);
     }
 
-    const device = await this.createDevice(properties, session);
-    const registeredDevice = await this.cloud.registerDevice(device);
-    return { type: 'registered', data: registeredDevice };
+    const device = await this.createDevice(session, properties);
+    await this.cloud.broadcastMessage(session.credentials, 'register', { device });
+    return { type: 'registered', data: device };
   }
 
-  async createDevice(properties, session) {
-    const { name, type } = properties;
+  async createDevice(session, properties) {
+    const { type, name, active } = properties;
     if (!type) {
       this.throwError('\'type\' is required', 400);
     }
 
     let device;
 
-    if (type === 'gateway' || type === 'app') {
-      device = await this.createAppOrGatewayDevice(session, name, type);
+    if (type === 'app') {
+      device = await this.registerApp(session, { name });
+    } else if (type === 'gateway') {
+      device = await this.registerGateway(session, { name, active });
     } else {
       this.throwError('\'type\' should be \'gateway\' or \'app\'', 400);
     }
@@ -32,32 +36,10 @@ class RegisterDevice {
     return device;
   }
 
-  async createAppOrGatewayDevice(session, name, type) {
-    if (!await this.isSessionOwnerUser(session)) {
+  verifyPermissions(authenticatedDevice) {
+    if (authenticatedDevice.type !== 'knot:user') {
       this.throwError('Device owner isn\'t a user', 400);
     }
-
-    const device = this.createBasicDevice(name, type);
-    device.meshblu = {
-      version: '2.0.0',
-      whitelists: this.generateWhitelists(session.credentials.uuid),
-    };
-
-    return device;
-  }
-
-  createBasicDevice(name, type) {
-    return {
-      type,
-      metadata: {
-        name: name || '',
-      },
-    };
-  }
-
-  async isSessionOwnerUser(session) {
-    const deviceOwner = await this.cloud.getDevice(session.credentials, session.credentials.uuid);
-    return deviceOwner.type === 'knot:user';
   }
 
   throwError(message, code) {
@@ -66,25 +48,124 @@ class RegisterDevice {
     throw error;
   }
 
-  generateWhitelists(ownerUuid) {
-    return {
-      discover: {
-        as: [{
-          uuid: ownerUuid,
-        }],
-        view: [{
-          uuid: ownerUuid,
-        }],
+  async registerApp(session, options) {
+    const user = await this.cloud.getDevice(session.credentials, session.credentials.uuid);
+    this.verifyPermissions(user);
+
+    const app = await this.createApp(user, options);
+    await this.connectRouterToApp(session, user, app);
+
+    return app;
+  }
+
+  async registerGateway(session, options) {
+    const user = await this.cloud.getDevice(session.credentials, session.credentials.uuid);
+    this.verifyPermissions(user);
+
+    const gateway = await this.createGateway(user, options);
+    await this.connectRouterToGateway(session, user, gateway);
+
+    return gateway;
+  }
+
+  async createApp(user, options) {
+    return this.cloud.registerDevice({
+      type: 'app',
+      metadata: {
+        name: options.name,
       },
-      configure: {
-        as: [{
-          uuid: ownerUuid,
-        }],
-        update: [{
-          uuid: ownerUuid,
-        }],
+      knot: {
+        router: user.knot.router,
       },
-    };
+      meshblu: {
+        version: '2.0.0',
+        whitelists: {
+          discover: {
+            view: [{ uuid: user.uuid }],
+          },
+          configure: {
+            update: [{ uuid: user.uuid }],
+          },
+        },
+      },
+    });
+  }
+
+  async createGateway(user, options) {
+    return this.cloud.registerDevice({
+      type: 'gateway',
+      metadata: {
+        name: options.name,
+      },
+      knot: {
+        user: user.uuid,
+        router: user.knot.router,
+        active: options.active || false,
+      },
+      meshblu: {
+        version: '2.0.0',
+        whitelists: {
+          discover: {
+            view: [{ uuid: user.uuid }],
+          },
+          configure: {
+            update: [{ uuid: user.uuid }],
+          },
+        },
+      },
+    });
+  }
+
+  async connectRouterToApp(session, user, app) {
+    await this.givePermission(session, user.knot.router, app.uuid, 'broadcast.received');
+    await this.givePermission(session, user.knot.router, app.uuid, 'unregister.received');
+    await this.givePermission(session, user.knot.router, app.uuid, 'configure.received');
+
+    await this.subscribeOwn(session, app.uuid, 'broadcast.received');
+    await this.subscribeOwn(session, app.uuid, 'unregister.received');
+    await this.subscribe(session, user.knot.router, app.uuid, 'broadcast.received');
+    await this.subscribe(session, user.knot.router, app.uuid, 'unregister.received');
+
+    await this.givePermission(session, user.knot.router, app.uuid, 'message.as');
+    await this.givePermission(session, user.knot.router, app.uuid, 'discover.as');
+    await this.givePermission(session, user.knot.router, app.uuid, 'configure.as');
+  }
+
+  async connectRouterToGateway(session, user, gateway) {
+    await this.givePermission(session, user.knot.router, gateway.uuid, 'configure.update');
+  }
+
+  async subscribe(session, from, to, type) {
+    await this.cloud.createSubscription(session.credentials, {
+      subscriberUuid: to,
+      emitterUuid: from,
+      type,
+    });
+  }
+
+  async subscribeOwn(session, uuid, type) {
+    await this.subscribe(session, uuid, uuid, type);
+  }
+
+  async givePermission(session, from, to, type) {
+    const device = await this.cloud.getDevice(session.credentials, from);
+    this.pushToWhitelist(device, type, to);
+    await this.cloud.updateDevice(session.credentials, device.uuid, { meshblu: device.meshblu });
+  }
+
+  pushToWhitelist(device, type, uuid) {
+    _.defaultsDeep(device, { meshblu: { whitelists: this.pathToObject(type) } });
+    _.get(device, `meshblu.whitelists.${type}`).push({ uuid });
+  }
+
+  pathToObject(path) {
+    /* eslint-disable no-multi-spaces */
+    return _.chain(path)                                // 'broadcast.received'
+      .toPath()                                         // ['broadcast', 'received']
+      .reverse()                                        // ['received', 'broadcast']
+      .reduce((acc, step) => _.set({}, step, acc), [])  // { broadcast: { received: [] }}
+      .value();
+    /* eslint-enable no-multi-spaces */
   }
 }
 
