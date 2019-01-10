d00f6ab0a1486bace774330d39cb87c1c3685dde
Ramon Ribeiro <rhpr@cesar.org.br>
2019-01-10 16:44:03 -0300

    Add create session token handler
    Change-Id: Idfc6f65d1ca04575c3e07fb536cfd488074c49a8
    Reviewed-on: https://review.gerrithub.io/c/440082
    Reviewed-by: João Neto <joao.alexandre@cesar.org.br>
    Reviewed-by: Paulo Serra Filho <ptvsf@cesar.org.br>
    Tested-by: Paulo Serra Filho <ptvsf@cesar.org.br>

---
+++ src/interactors/CreateSessionToken.js
@@ -0,0 +1,26 @@
+class CreateSessionToken {
+  constructor(sessionStore, cloud) {
+    this.sessionStore = sessionStore;
+    this.cloud = cloud;
+  }
+
+  async execute(requestId, data) {
+    const { credentials } = this.sessionStore.get(requestId);
+    if (!credentials) {
+      this.throwError('Unauthorized', 401);
+    }
+
+    // TODO: Verify the property createdAt to expire the token
+    const { token } = await this.cloud.createSessionToken(credentials, data.uuid);
+
+    return { type: 'created', data: token };
+  }
+
+  throwError(message, code) {
+    const error = new Error(message);
+    error.code = code;
+    throw error;
+  }
+}
+
+export default CreateSessionToken;
