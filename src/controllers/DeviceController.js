f6db2d81315277e413ab29f51758e9fc2a1f6c95
Ramon Ribeiro <rhpr@cesar.org.br>
2019-01-17 10:16:58 -0300

    Add activate handler
    Change-Id: I80e764e08bc831c15f0bfe2e49f4dced0e1495a1

--- src/controllers/DeviceController.js
+++ src/controllers/DeviceController.js
@@ -6,6 +6,7 @@ class DeviceController {
     unregisterInteractor,
     updateSchemaInteractor,
     createSessionTokenInteractor,
+    activateInteractor,
     logger,
   ) {
     this.updateMetadataInteractor = updateMetadataInteractor;
@@ -14,6 +15,7 @@ class DeviceController {
     this.unregisterInteractor = unregisterInteractor;
     this.updateSchemaInteractor = updateSchemaInteractor;
     this.createSessionTokenInteractor = createSessionTokenInteractor;
+    this.activateInteractor = activateInteractor;
     this.logger = logger;
   }
 
@@ -94,6 +96,20 @@ class DeviceController {
       await responseHandler.send(response.type, response.data);
     } catch (error) {
       this.logger.error(`Failed in create token (${error.code || 500}): ${error.message}`);
+      await responseHandler.send('error', {
+        code: error.code || 500,
+        message: error.message,
+      });
+    }
+  }
+
+  async activateDevice(request, responseHandler) {
+    try {
+      const response = await this.activateInteractor.execute(request.id, request.data);
+      this.logger.info('Device activated');
+      await responseHandler.send(response.type, response.data);
+    } catch (error) {
+      this.logger.error(`Failed in activate device (${error.code || 500}): ${error.message}`);
       await responseHandler.send('error', {
         code: error.code || 500,
         message: error.message,
---
+++ src/interactors/ActivateDevice.js
@@ -0,0 +1,28 @@
+class ActivateDevice {
+  constructor(sessionStore, cloud) {
+    this.sessionStore = sessionStore;
+    this.cloud = cloud;
+  }
+
+  async execute(requestId, data) {
+    const { credentials } = this.sessionStore.get(requestId);
+    if (!credentials) {
+      this.throwError('Unauthorized', 401);
+    }
+
+    await this.cloud.updateDevice(credentials, data.uuid, { knot: { active: true } });
+    return { type: 'activated' };
+  }
+
+  isSessionOwnerUser(authenticatedDevice) {
+    return authenticatedDevice.type === 'knot:user';
+  }
+
+  throwError(message, code) {
+    const error = new Error(message);
+    error.code = code;
+    throw error;
+  }
+}
+
+export default ActivateDevice;
