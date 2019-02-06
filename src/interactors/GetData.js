bdb441e0b3345ac93650912f53be33a28998159b
João Neto <joao.alexandre@cesar.org.br>
2019-02-06 17:57:58 -0300

    Add getData handler
    Change-Id: I32dbe1dcf5689dba94a57dd7d6fbb34befadac60

---
+++ src/interactors/GetData.js
@@ -0,0 +1,34 @@
+class GetData {
+  constructor(sessionStore, cloud) {
+    this.sessionStore = sessionStore;
+    this.cloud = cloud;
+  }
+
+  async execute(requestId, data) {
+    const { credentials } = this.sessionStore.get(requestId);
+    if (!credentials) {
+      this.throwError('Unauthorized', 401);
+    }
+
+    if (!await this.isSessionOwnerApp(credentials)) {
+      this.throwError('Only apps can send \'get-data\'', 400);
+    }
+
+    await this.cloud.updateDevice(credentials, data.id, { get_data: data.sensorIds });
+    await this.cloud.broadcastMessage(credentials, 'command', { get_data: data });
+    return { type: 'sent' };
+  }
+
+  throwError(message, code) {
+    const error = new Error(message);
+    error.code = code;
+    throw error;
+  }
+
+  async isSessionOwnerApp(credentials) {
+    const device = await this.cloud.getDevice(credentials, credentials.uuid);
+    return device.type === 'app';
+  }
+}
+
+export default GetData;
