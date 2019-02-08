1b0b4596434ef9d80e4637555e95d01cefaad10d
Ramon Ribeiro <rhpr@cesar.org.br>
2019-02-08 13:40:54 -0300

    Add publish data handler
    Change-Id: Iee4fad0e782087a0f30eb560a78593ffb8fba722

---
+++ src/interactors/PublishData.js
@@ -0,0 +1,71 @@
+import isBase64 from 'is-base64';
+
+class PublishData {
+  constructor(sessionStore, cloud) {
+    this.sessionStore = sessionStore;
+    this.cloud = cloud;
+  }
+
+  async execute(requestId, data) {
+    const session = this.sessionStore.get(requestId);
+
+    if (!session) {
+      this.throwError('Unauthorized', 401);
+    }
+    const device = await this.cloud.getDevice(session.credentials, session.credentials.uuid);
+
+    if (!device.type === 'thing') {
+      this.throwError('Only things can publish data', 400);
+    }
+
+    if (!device.schema) {
+      this.throwError(`The thing ${device.id} has no schema for sensors`, 403);
+    }
+
+    this.validateValueType(device, data);
+
+    await this.cloud.broadcastMessage(session.credentials, 'data', { data });
+    return { type: 'published' };
+  }
+
+  validateValueType(device, data) {
+    const schema = device.schema.find(value => value.sensor_id === data.sensor_id);
+
+    if (!schema) {
+      this.throwError(`The thing ${device.id} has no schema for sensor id ${data.sensor_id}`, 403);
+    }
+
+    switch (schema.value_type) {
+      case 1: // INTEGER
+        if (!(data.value instanceof Number) && !Number.isInteger(data.value)) {
+          this.throwError('Value is not integer', 403);
+        }
+        break;
+      case 2: // FLOAT
+        if (!(data.value instanceof Number) && Number.isInteger(data.value)) {
+          this.throwError('Value is not float', 403);
+        }
+        break;
+      case 3: // BOOL
+        if (!(typeof data.value === 'boolean')) {
+          this.throwError('Value is not boolean', 403);
+        }
+        break;
+      case 4: // RAW
+        if (!isBase64(data.value)) {
+          this.throwError('Value is not in base 64', 403);
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  throwError(message, code) {
+    const error = Error(message);
+    error.code = code;
+    throw error;
+  }
+}
+
+export default PublishData;
